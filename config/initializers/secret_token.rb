e2c5039cf4148ad2e52939010d7c841be53c8bb8
AndresArismendi <andres.arismendi@usach.cl>
2015-08-12 23:52:10 -0300

    proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funciona::Application.config.secret_key_base = 'e5375cab755ae83283bc7a37c844bccecbdffdb9c13e36f2987e0003dd00f020d9dc787bf648eb451fa4af2dc8f322b6fec09d6eb58b0c8a4ddaa054828dc3f7'
